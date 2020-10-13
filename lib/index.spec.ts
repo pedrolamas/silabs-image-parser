759697c848823c69cb4c8cfcbacd67954c905e84
Pedro Lamas <pedrolamas@gmail.com>
2020-10-13 14:33:38 +0100

    Replaces jest with mocha

---
+++ lib/index.spec.ts
@@ -0,0 +1,51 @@
+import fs from 'fs';
+import path from 'path';
+import zhc from 'zigbee-herdsman-converters/ota/common';
+import { expect } from 'chai';
+
+import { getFormat, eblParser, gblParser } from '.';
+
+type OtaImage = [
+  string,
+  {
+    format: string;
+  }
+];
+
+const otaImages: OtaImage[] = [
+  ['10005777-4.1-TRADFRI-control-outlet-2.0.024.ota.ota.signed', { format: 'gbl' }],
+  ['10035514-2.1-TRADFRI-bulb-ws-2.3.050.ota.ota.signed', { format: 'ebl' }],
+];
+
+describe('Parse images', () => {
+  otaImages.forEach((otaImage) => {
+    const [filename, meta] = otaImage;
+
+    it(`Can parse "${filename}"`, () => {
+      const data = fs.readFileSync(path.join(__dirname, '__otaImageFiles__', filename));
+
+      // fix for IKEA OTA images
+      const start = data.readUInt32LE(0) === 0x5349474e ? 12 + data.readUInt16LE(12) : 0;
+
+      const image = zhc.parseImage(data.slice(start));
+
+      image.elements.forEach((element: { data: Buffer }) => {
+        const format = getFormat(element.data);
+
+        expect(format).to.equal(meta.format);
+
+        switch (format) {
+          case 'gbl':
+            expect(gblParser.parse(element.data)).to.not.be.undefined;
+
+            break;
+
+          case 'ebl':
+            expect(eblParser.parse(element.data)).to.not.be.undefined;
+
+            break;
+        }
+      });
+    });
+  });
+});
